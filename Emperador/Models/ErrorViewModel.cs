e4258e953c1ec7ef2b55eedc862109bd80510cf9
luisdrz20 <luisdavidrs002@gmail.com>
2024-05-26 08:52:00 -0600

    PrimerGuardado

---
+++ Emperador/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Emperador.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
